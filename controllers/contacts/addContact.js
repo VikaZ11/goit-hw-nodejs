7e4c1302ceaeac9e9443005b1c929391dc323caf
VikaZ11 <viktoriia.zahorodnia@gmail.com>
2023-02-22 11:13:09 +0100

    finish HW

---
+++ controllers/contacts/addContact.js
@@ -0,0 +1,16 @@
+const contactsOperations = require("../../models/contacts");
+
+const addContact = async (req, res) => {
+  const { name, email, phone } = req.body;
+
+  const result = await contactsOperations.addContact(name, email, phone);
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: {
+      result,
+    },
+  });
+};
+
+module.exports = addContact;
